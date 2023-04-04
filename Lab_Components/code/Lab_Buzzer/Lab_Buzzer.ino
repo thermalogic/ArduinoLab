f30512a0cc55d82df3de326f017925c1dfd2b68a
thermalogic <cmh@seu.edu.cn>
2023-04-04 13:37:47 +0800

    buzzer

---
+++ Lab_Components/code/Lab_Buzzer/Lab_Buzzer.ino
@@ -0,0 +1,35 @@
+/*********************************
+ * name:buzzer
+ * function: you should hear the buzzer make sounds.
+ *************************************/
+//Email: info@primerobotics.in
+//Website: www.rimerobotics.in
+/************************************/
+int buzzer = 12;//the pin of the active buzzer
+void setup()
+{
+  pinMode(buzzer,OUTPUT);//initialize the buzzer pin as an output
+}
+void loop()
+{
+  unsigned char i;
+  while(1)
+  {
+    //output an frequency
+    for(i=0;i<80;i++)
+    {
+      digitalWrite(buzzer,HIGH);
+      delay(1);//wait for 1ms
+      digitalWrite(buzzer,LOW);
+      delay(1);//wait for 1ms
+    }
+    //output another frequency
+    for(i=0;i<100;i++)
+    {
+      digitalWrite(buzzer,HIGH);
+      delay(2);//wait for 2ms
+      digitalWrite(buzzer,LOW);
+      delay(2);//wait for 2ms
+    }
+  }
+}
