622745ae480def2819771ad6e175f3e8f3bf7023
thermalogic <cmh@seu.edu.cn>
2023-04-08 22:55:30 +0800

    t

--- Lab_ToyCar/ToyCar_Motor/ToyCar_Motor.ino
+++ Lab_ToyCar/ToyCar_Motor/ToyCar_Motor.ino
@@ -1,35 +1,39 @@
 /*
  Lab ToyCar motor
+ Main Motor - green ->D1 ->p1-> Arduino D9  
+ Main Motor - black ->D2 ->p2-> Arduino D10 
+ 
+ Turn Motor - green ->D1 ->p1-> Arduino D9 
 
 */
 
-#define forwardPWM 10
-#define backPWM 9
+#define forwardPWM 9
+#define backPWM 10
 
 #define turn_leftPWM 5
 #define turn_rightPWM 6
 
-int speed_level = 50;
+int speed_level = 250;
 
 void setup()
 {
   pinMode(forwardPWM, OUTPUT);
   pinMode(backPWM, OUTPUT);
-  digitalWrite(forwardPWM, LOW);
+  /*igitalWrite(forwardPWM, LOW);
   digitalWrite(backPWM, LOW);
   delay(1000);
   analogWrite(forwardPWM, speed_level);
   analogWrite(backPWM, 0);
-  delay(1000);
+  delay(2000);
   analogWrite(forwardPWM, 0);
-  analogWrite(backPWM, 100);
-  delay(1000);
+  analogWrite(backPWM, speed_level);
+  delay(2000);
   // digitalWrite(forwardPWM, LOW);
   // digitalWrite(backPWM ,HIGH);
   // delay(1000);
   analogWrite(forwardPWM, 0);
   analogWrite(backPWM, 0);
-  delay(1000);
+  delay(2000);
   /*
   pinMode(turn_leftPWM, OUTPUT);
   pinMode(turn_rightPWM, OUTPUT);
@@ -52,4 +56,11 @@ void setup()
 
 void loop()
 {
+  analogWrite(forwardPWM, 0);
+  analogWrite(backPWM, speed_level);
+  delay(2000);
+  analogWrite(forwardPWM,speed_level);
+  analogWrite(backPWM, 0);
+  delay(2000);
+  
 }
