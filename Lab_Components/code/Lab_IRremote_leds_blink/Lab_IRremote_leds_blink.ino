4fe5f455741688c3da4570b63498e0f90d573255
thermalogic <cmh@seu.edu.cn>
2023-07-01 14:38:32 +0800

    test_FreeRTOS

--- Lab_Components/code/Lab_IRremote_leds_blink/Lab_IRremote_leds_blink.ino
+++ Lab_Components/code/Lab_IRremote_leds_blink/Lab_IRremote_leds_blink.ino
@@ -7,7 +7,7 @@
 #include "PinDefinitionsAndMore.h"  // Define macros for input and output pin etc.
 
 #define DECODE_NEC  // Includes Apple and Onkyo
-#define IR_RECEIVE_PIN 2
+#define ACTION_RECEIVE_PIN 2
 
 #define LED_LEFT_PIN 5
 #define LED_RIGHT_PIN 6
@@ -17,13 +17,13 @@ long previousMillis_blink;  // for led blink
 const int interval_blink = 200;
 int led_left_state, led_right_state;  // led_state used to set the LED
 
-#define DEV_GO 0
-#define DEV_BACK 1
-#define DEV_STOP 2
-#define DEV_LEFT 3
-#define DEV_RIGHT 4
+#define ACTION_GO 0x47 //ZTE
+#define ACTION_LEFT 0x48
+#define ACTION_STOP 0x49
+#define ACTION_RIGHT 0x4A
+#define ACTION_BACK 0x4B
 
-int state = DEV_STOP;
+int state = ACTION_STOP;
 
 int update_led_state(int led_pin, int led_state) {
   if (led_state == LOW) {
@@ -53,14 +53,14 @@ void setup() {
 
   Serial.begin(9600);
   Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
-  IrReceiver.begin(IR_RECEIVE_PIN,  DISABLE_LED_FEEDBACK);
+  IrReceiver.begin(ACTION_RECEIVE_PIN,  DISABLE_LED_FEEDBACK);
   Serial.print(F("Ready to receive IR signals of protocols: "));
   printActiveIRProtocols(&Serial);
-  Serial.println(F("at pin " STR(IR_RECEIVE_PIN)));
+  Serial.println(F("at pin " STR(ACTION_RECEIVE_PIN)));
 }
 
 void loop() {
-  if (state == DEV_BACK) {
+  if (state == ACTION_BACK) {
     led_blink();
   }
   if (IrReceiver.decode()) {
@@ -75,33 +75,30 @@ void loop() {
 
     IrReceiver.resume();  // Enable receiving of the next value
 
-    if (IrReceiver.decodedIRData.command == 0x11) {
-      // forward - 2
-      state = DEV_GO;
+    if (IrReceiver.decodedIRData.command == ACTION_GO) {
+      state = ACTION_GO;
       digitalWrite(LED_LEFT_PIN, HIGH);
       digitalWrite(LED_RIGHT_PIN, HIGH);
 
-    } else if (IrReceiver.decodedIRData.command == 0x14) {
+    } else if (IrReceiver.decodedIRData.command ==  ACTION_LEFT) {
       // DEV_LEFT - 4
-      state = DEV_LEFT;
+      state = ACTION_LEFT;
       digitalWrite(LED_LEFT_PIN, HIGH);
       digitalWrite(LED_RIGHT_PIN, LOW);
-    } else if (IrReceiver.decodedIRData.command == 0x16) {
+    } else if (IrReceiver.decodedIRData.command == ACTION_RIGHT) {
       // right - 6
-      state = DEV_RIGHT;
+      state = ACTION_RIGHT;
       digitalWrite(LED_LEFT_PIN, LOW);
       digitalWrite(LED_RIGHT_PIN, HIGH);
-    } else if (IrReceiver.decodedIRData.command == 0x19) {
-      // DEV_BACK
-      state = DEV_BACK;
+    } else if (IrReceiver.decodedIRData.command == ACTION_BACK) {
+      state = ACTION_BACK;
       previousMillis_blink = 0;
       led_left_state = LOW;
       led_right_state = LOW;
       digitalWrite(LED_LEFT_PIN, led_left_state);
       digitalWrite(LED_RIGHT_PIN, led_right_state);
-    } else if (IrReceiver.decodedIRData.command == 0x15) {
-      // DEV_STOP
-      state = DEV_STOP;
+    } else if (IrReceiver.decodedIRData.command ==  ACTION_STOP) {
+      state = ACTION_STOP;
       brightness = 20;
       analogWrite(LED_LEFT_PIN, brightness);
       analogWrite(LED_RIGHT_PIN, brightness);
