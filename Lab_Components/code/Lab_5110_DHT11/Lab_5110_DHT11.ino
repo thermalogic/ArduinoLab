a0cb45e6cb190247614c8e7ceb0597af9e2a1bba
thermalogic <cmh@seu.edu.cn>
2024-03-09 00:41:00 +0800

    5110

--- Lab_Components/code/Lab_5110_DHT11/Lab_5110_DHT11.ino
+++ Lab_Components/code/Lab_5110_DHT11/Lab_5110_DHT11.ino
@@ -2,20 +2,19 @@
  * Interfacing Arduino with Nokia 5110 LCD and DHT11 sensor.
  * This is a free software with NO WARRANTY.
  */
- 
+
 #include <SPI.h>               // include SPI library
 #include <Adafruit_GFX.h>      // include adafruit graphics library
 #include <Adafruit_PCD8544.h>  // include adafruit PCD8544 (Nokia 5110) library
-           // include DHT library code
+// include DHT library code
 #include <DFRobot_DHT11.h>
 DFRobot_DHT11 DHT;
 #define DHT11_PIN 10
 
 // Nokia 5110 LCD module connections (CLK, DIN, D/C, CS, RST)
 Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
-  
-void setup()
-{
+
+void setup() {
   Serial.begin(9600);
   delay(1000);  // wait 1 second
   // initialize the display
@@ -23,7 +22,7 @@ void setup()
   // you can change the contrast around to adapt the display
   // for the best viewing!
   display.setContrast(55);
-  display.clearDisplay();   // clear the screen and buffer
+  display.clearDisplay();  // clear the screen and buffer
   display.drawFastHLine(0, 23, display.width(), BLACK);
   display.setTextSize(1);
   display.setTextColor(BLACK, WHITE);
@@ -31,17 +30,16 @@ void setup()
   display.print("TEMPERATURE:");
   display.setCursor(56, 12);
   display.print("C");
- 
+
   display.setCursor(15, 28);
   display.print("HUMIDITY:");
-  display.setCursor(56,40);
+  display.setCursor(56, 40);
   display.print("%");
   display.display();
 }
- 
+
 // main loop
-void loop()
-{
+void loop() {
   char strhumidity[5];
   char strTemperature[5];
   DHT.read(DHT11_PIN);
@@ -49,21 +47,19 @@ void loop()
   Serial.print(DHT.temperature);
   Serial.print("  humi:");
   Serial.println(DHT.humidity);
- 
+
   // print temperature
-  dtostrf(DHT.temperature, 5, 2, strTemperature); 
+  dtostrf(DHT.temperature, 5, 2, strTemperature);
   display.setCursor(24, 12);
   display.print(strTemperature);
- 
+
   // print humidity
-  dtostrf(DHT.humidity, 5, 2, strhumidity);     
+  dtostrf(DHT.humidity, 5, 2, strhumidity);
   display.setCursor(24, 40);
   display.print(strhumidity);
- 
- 
- // now update the display
+
+  // now update the display
   display.display();
-  delay(100);  // wait 1 second
- 
+  delay(1000);  // wait 1 second
 }
 // end of code.
