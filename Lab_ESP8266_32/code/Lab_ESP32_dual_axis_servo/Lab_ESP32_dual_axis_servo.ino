5b36e18c52d87a162ad4ac1912b4a5a20d01fd3a
thermalogic <cmh@seu.edu.cn>
2024-04-03 12:29:09 +0800

    servo

--- Lab_ESP8266_32/code/Lab_ESP32_dual_axis_servo/Lab_ESP32_dual_axis_servo.ino
+++ Lab_ESP8266_32/code/Lab_ESP32_dual_axis_servo/Lab_ESP32_dual_axis_servo.ino
@@ -76,9 +76,9 @@
 Servo vertical_servo;
 Servo horizontal_servo;
 // 16 servo objects can be created on the ESP32
-M
-int pos = 0;    // variable to store the servo position
-// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
+
+int pos = 0;  // variable to store the servo position
+// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33
 // Possible PWM GPIO pins on the ESP32-S2: 0(used by on-board button),1-17,18(used by on-board LED),19-21,26,33-42
 // Possible PWM GPIO pins on the ESP32-S3: 0(used by on-board button),1-21,35-45,47,48(used by on-board LED)
 // Possible PWM GPIO pins on the ESP32-C3: 0(used by on-board button),1-7,8(used by on-board LED),9-10,18-21
@@ -88,71 +88,72 @@ int servoPin = 17;
 int servoPin = 7;
 #else
 int vertical_servo_pin = 2;
-int horizontal_servo_pin=15;
+int horizontal_servo_pin = 15;
 #endif
 
-void servo_turning()
-{
-	for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-		vertical_servo.write(pos);    // tell servo to go to position in variable 'pos'
-		delay(15);             // waits 15ms for the servo to reach the position
-	}
+void vertical_servo_turning() {
+  for (pos = 90; pos >= 0; pos -= 1) {  // goes from 180 degrees to 0 degrees
+    vertical_servo.write(pos);          // tell servo to go to position in variable 'pos'
+    delay(15);                          // waits 15ms for the servo to reach the position
+  }
   delay(1000);
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-		// in steps of 1 degree
-		vertical_servo.write(pos);    // tell servo to go to position in variable 'pos'
-		delay(15);             // waits 15ms for the servo to reach the position
-	}
-	  delay(1000);
-  for (pos = 180; pos >= 90; pos -= 1) { // goes from 0 degrees to 180 degrees
-		// in steps of 1 degree
-		vertical_servo.write(pos);    // tell servo to go to position in variable 'pos'
-		delay(15);             // waits 15ms for the servo to reach the position
-	}
+  for (pos = 0; pos <= 180; pos += 1) {  // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    vertical_servo.write(pos);  // tell servo to go to position in variable 'pos'
+    delay(15);                  // waits 15ms for the servo to reach the position
+  }
   delay(1000);
+  for (pos = 180; pos >= 90; pos -= 1) {  // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    vertical_servo.write(pos);  // tell servo to go to position in variable 'pos'
+    delay(15);                  // waits 15ms for the servo to reach the position
+  }
+  delay(1000);
+}
 
-//
-	for (pos = 45; pos >= 25; pos -= 1) { // goes from 180 degrees to 0 degrees
-		horizontal_servo.write(pos);    // tell servo to go to position in variable 'pos'
-		delay(15);             // waits 15ms for the servo to reach the position
-	}
-  delay(1000);
-  for (pos = 25; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-		// in steps of 1 degree
-		horizontal_servo.write(pos);    // tell servo to go to position in variable 'pos'
-		delay(15);             // waits 15ms for the servo to reach the position
-	}
-	  delay(1000);
-  //for (pos = 180; pos >=45; pos -= 1) { // goes from 0 degrees to 180 degrees
-		// in steps of 1 degree
-//		horizontal_servo.write(pos);    // tell servo to go to position in variable 'pos'
-//		delay(15);             // waits 15ms for the servo to reach the position
-//	}
+void horizontal_servo_turning(){
+
+  for (pos = 45; pos >= 25; pos -= 1) {  // goes from 180 degrees to 0 degrees
+  horizontal_servo.write(pos);           // tell servo to go to position in variable 'pos'
+  delay(15);                             // waits 15ms for the servo to reach the position
+}
+delay(1000);
+for (pos = 25; pos <= 170; pos += 1) {  // goes from 0 degrees to 180 degrees
+  // in steps of 1 degree
+  horizontal_servo.write(pos);  // tell servo to go to position in variable 'pos'
+  delay(15);                    // waits 15ms for the servo to reach the position
+}
+delay(1000);
+
+for (pos = 170; pos >= 45; pos -= 1) {  // goes from 0 degrees to 180 degrees
+  // in steps of 1 degree
+  horizontal_servo.write(pos);  // tell servo to go to position in variable 'pos'
+  delay(15);                    // waits 15ms for the servo to reach the position
+}
 }
 
 void setup() {
-	//Allow allocation of all timers
-	ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
+  //Allow allocation of all timers
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
 
-  vertical_servo.setPeriodHertz(FREQUENCY);    // standard 50 hz servo
-  vertical_servo.attach(vertical_servo_pin, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH); // attaches the servo on pin 18 to the servo object
-	vertical_servo.write(90);    // tell servo to go to position in variable 'pos'
-	
-  horizontal_servo.setPeriodHertz(FREQUENCY);    // standard 50 hz servo
-  horizontal_servo.attach(horizontal_servo_pin, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH); // attaches the servo on pin 18 to the servo object
-  horizontal_servo.write(45);    // tell servo to go to position in variable 'pos'
-	
+  vertical_servo.setPeriodHertz(FREQUENCY);                                     // standard 50 hz servo
+  vertical_servo.attach(vertical_servo_pin, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);  // attaches the servo on pin 18 to the servo object
+  vertical_servo.write(90);                                                     // tell servo to go to position in variable 'pos'
+
+  horizontal_servo.setPeriodHertz(FREQUENCY);                                       // standard 50 hz servo
+  horizontal_servo.attach(horizontal_servo_pin, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);  // attaches the servo on pin 18 to the servo object
+  horizontal_servo.write(45);                                                       // tell servo to go to position in variable 'pos'
+
   // using default min/max of 1000us and 2000us
-	// different servos may require different min/max settings
-	// for an accurate 0 to 180 sweep
+  // different servos may require different min/max settings
+  // for an accurate 0 to 180 sweep
   delay(2000);
- servo_turning();
+  vertical_servo_turning();
+  horizontal_servo_turning();
 }
 
 void loop() {
-  
 }
-
